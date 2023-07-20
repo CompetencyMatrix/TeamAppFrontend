8baab9353f8d0a554c119eb5c74e40349f764216
filip <filip.pawlowski001@gmail.com>
2023-07-20 14:50:37 +0200

    Implemented employees-component.

---
+++ src/app/features/employees/employees.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Employee } from '../../models/employee';
+
+@Component({
+  selector: 'app-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.scss']
+})
+export class EmployeesComponent {
+  employee: Employee = {
+    id: "123DAF",
+    name: "Gregory"
+  };
+}
